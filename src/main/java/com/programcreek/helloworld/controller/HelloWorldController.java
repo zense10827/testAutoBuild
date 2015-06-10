a9ab4c6cd34e1b1ed88e00dd124520cdab3b0c84
zense10827 <5510613424@student.tu.ac.th>
2015-06-10 17:35:45 +0700

    test 8

--- src/main/java/com/programcreek/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/programcreek/helloworld/controller/HelloWorldController.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.ModelAndView;
  
 @Controller
 public class HelloWorldController {
-	String message = "Welcome to My Test !!!!!!!";
+	String message = "Welcome to My Test!";
  
 	@RequestMapping("/hello")
 	public ModelAndView showMessage(
